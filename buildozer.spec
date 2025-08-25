a59a3d6b05ea79fdbb00fa9af7ab8237be2ce0e1
mohamedgadallahabdullah <mohamedgadallh11aa@gmail.com>
2025-08-25 04:25:41 +0300

    Add files via upload

---
+++ buildozer.spec
@@ -0,0 +1,14 @@
+[app]
+title = MyKivyApp
+package.name = mykivyapp
+package.domain = org.test
+source.dir = .
+source.include_exts = py, kv
+version = 1.0
+requirements = python3, kivy==2.3.0, kivymd
+orientation = portrait
+fullscreen = 0
+
+[buildozer]
+log_level = 2
+warn_on_root = 1
---
+++ main.py
@@ -0,0 +1,18 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Blue"
+        return Builder.load_file("main.kv")
+
+    def log(self):
+        self.root.ids.wl.text = f"Welcome {self.root.ids.usr.text}"
+
+    def clean(self):
+        self.root.ids.wl.text = "Welcome"
+        self.root.ids.usr.text = ""
+        self.root.ids.passwd.text = ""
+
+MainApp().run()
