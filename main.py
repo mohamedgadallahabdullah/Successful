70298b221fed3f10225b6caeb4a3d7c363c0234e
mohamedgadallahabdullah <mohamedgadallh11aa@gmail.com>
2025-09-28 19:14:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,21 @@
-import arabic_reshaper
-from bidi.algorithm import get_display
+from kivy.lang import Builder
+from kivymd.app import MDApp
 
-from random import choice
-from kivy.app import App
-from kivy.uix.button import Button, Label
-from kivy.core.window import Window
-from kivy.uix.boxlayout import BoxLayout
-from kivy.utils import get_color_from_hex
-
-
-Window.clearcolor = (1, 0, 0, 1)
-
-class Name(App):
+class MainApp(MDApp):
     def build(self):
-        self.h = 0  # تعريف h كمتغير خاص بالكائن
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Blue"
         
-        layout = BoxLayout(orientation="horizontal")
-        tex2 = arabic_reshaper.reshape("التالي")
-        tex3 = get_display(tex2)
-        
-        self.b1 = Button(
-            text=tex3,
-            font_name="arial-1.ttf",
-            size_hint=(None, None),
-            size=(200, 70),
-            on_press=self.ching,
-            pos_hint={'x': 0.4, 'y': 0.01},
-            font_size=40,
-            halign="center",
-            valign="center"
-            
-        )
-        
-        self.List_of_dhikr = [
-            "             سبحان الله",
-            "              الحمد لله",
-            "                لا إله إلا الله",
-            "               الله أكبر",
-            "                 لا حول ولا قوة إلا بالله"
-        ]
-        
-        tex = arabic_reshaper.reshape(self.List_of_dhikr[self.h])
-        tex2 = get_display(tex)
-        
-        self.L1 = Label(
-            text=tex2,
-            font_name="arial-1.ttf",
-            font_size=42,
-            pos_hint={'x':0.5, 'y': 0.05},
-            halign="center",
-            valign="center"
-            
-            
-        )
-        self.L1.bind(size=self.L1.setter('text_size'))
-        
-        layout.add_widget(self.b1)
-        layout.add_widget(self.L1)
-        
-        return layout
+        return Builder.load_file("main.kv")
 
-    def ching(self, yes):
-        # تغيير الخلفية
-        list_color = ["#eb0e0e","#ff6803","#c4ff03","#0b03ff","#d30fff","#b5b5b5","#66ff8cff","#ffdf7fff"]
-        ching_color = choice(list_color)
-        Window.clearcolor = get_color_from_hex(ching_color)
-        
-        # تحديث النص
-        self.h = (self.h + 1) % len(self.List_of_dhikr)  # التنقل بين الأذكار
-        tex = arabic_reshaper.reshape(self.List_of_dhikr[self.h])
-        tex2 = get_display(tex)
-        self.L1.text = tex2
+    def log(self):
+        self.root.ids.wl.text = f'Welcome {self.root.ids.usr.text}'
 
+    def clean(self):
+        self.root.ids.wl.text = "Welcome"
+        self.root.ids.usr.text = " "
+        self.root.ids.passwd.text = ""
 
-if __name__ == "__main__":
-    Name().run()
+     
+ 
+MainApp().run()
